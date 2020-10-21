3553a7e24d85beee82daae030074b1c82f519593
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-22 05:51:06 +1100

    Play around with css in react in home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Recipe Blog</h1>
       </header>
+
+      <footer>
+        <div className="footer-parts">
+          <div className="social">
+            <a href="https://www.linkedin.com/in/syeda-ismat-farjana/">
+              LinkedIn
+            </a>
+            <a href="https://twitter.com/ismat_farjana">Twitter</a>
+            <a href="https://www.codewars.com/users/ismatfarjana">Code wars</a>
+          </div>
+          <div className="copyright">
+            Syeda Ismat Farjana &copy; 2020 MERN APP
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
